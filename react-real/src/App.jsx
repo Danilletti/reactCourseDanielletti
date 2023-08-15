12d070426181db8adee80fe4ebfbd0af0477d596
Danilletti <6bautistadaniel9@gmail.com>
2023-08-15 11:13:30 -0500

    1videoTerminado

--- react-real/src/App.jsx
+++ react-real/src/App.jsx
@@ -1,33 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 const addAt = (userName) => `@${userName}`
 
 export function App() {
+    const seguidos = ['JuanRuiz04', 'whatevercamps', 'espe0204']
+    const cuentas = ['JuanRuiz04', 'whatevercamps', 'espe0204', 'danilletti', 'blo2SonNT', 'Cm3va', 'CamilaQuimbaya']
+
+    const [name1, setName1] = useState('midudev')
+    const [follow1, setFollow1] = useState(false)
+    const [name2, setName2] = useState('blo2SonNT')
+    const [follow2, setFollow2] = useState(false)
+    const [name3, setName3] = useState('danilletti')
+    const [follow3, setFollow3] = useState(false)
+
+    const clickCito = () => {
+        const randomIndex1 = Math.floor(Math.random() * cuentas.length)
+        const randomIndex2 = Math.floor(Math.random() * cuentas.length)
+        const randomIndex3 = Math.floor(Math.random() * cuentas.length)
+        setName1(cuentas[randomIndex1])
+        setFollow1(seguidos.includes(cuentas[randomIndex1]))
+        setName2(cuentas[randomIndex2])
+        setFollow2(seguidos.includes(cuentas[randomIndex2]))
+        setName3(cuentas[randomIndex3])
+        setFollow3(seguidos.includes(cuentas[randomIndex3]))
+    }
+
+    useEffect(() => {
+        setFollow1(seguidos.includes(name1));
+    }, [name1]);
+    useEffect(() => {
+        setFollow2(seguidos.includes(name2));
+    }, [name2]);
+    useEffect(() => {
+        setFollow3(seguidos.includes(name3));
+    }, [name3]);
+
     return (
         <div className='app'>
             <TwitterFollowCard
+                initialIsFollowing={follow1}
                 addAt={addAt}
-                itsFollowing={true}
-                userName="danilletti"
-                name="Daniel Bautista"
-            />
-
-            <TwitterFollowCard
-                addAt={addAt}
-                itsFollowing={false}
-                userName="blo2SonNT"
-                name="Pepe "
-            />
-
-            <TwitterFollowCard
-                addAt={addAt}
-                itsFollowing={false}
-                userName="midudev"
+                userName={name1}
                 name="Miguel Perez"
             />
-        </div>
+            <TwitterFollowCard
+                initialIsFollowing={follow2}
+                addAt={addAt}
+                userName={name2}
+                name="Miguel Perez"
+            />
+            <TwitterFollowCard
+                initialIsFollowing={follow3}
+                addAt={addAt}
+                userName={name3}
+                name="Miguel Perez"
+            />
 
+            <button onClick={clickCito}>
+                Cambio de User
+            </button>
+        </div>
     )
 }
--- react-real/src/TwitterFollowCard.jsx
+++ react-real/src/TwitterFollowCard.jsx
@@ -1,10 +1,28 @@
-export function TwitterFollowCard({ addAt, userName, name, itsFollowing }) {
-    // const imagenSrc = `https://unavatar.io/${userName}`
-    const text = itsFollowing ? 'Siguiendo' : 'Seguir'
-    const buttonClassName = itsFollowing ? 'Siguiendo' : 'Seguir'
+import { useState, useEffect } from "react"
 
-    console.log(itsFollowing)
+export function TwitterFollowCard({ addAt, userName, name, initialIsFollowing }) {
+    // const imagenSrc = `https://unavatar.io/${userName}`
+
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
+    const [text, setText] = useState(isFollowing ? 'Siguiendo' : 'Seguir')
+    const [buttonClassName, setButtonClassName] = useState(isFollowing ? 'tw-followCard-button isFollowing' : 'tw-followCard-button')
+
+    useEffect(() => {
+        setIsFollowing(initialIsFollowing)
+    }, [initialIsFollowing])
+
+    useEffect(() => {
+        setText(isFollowing ? 'Siguiendo' : 'Seguir')
+        setButtonClassName(isFollowing ? 'tw-followCard-button isFollowing' : 'tw-followCard-button')
+    }, [isFollowing])
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+    console.log(isFollowing)
     return (
+
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img
@@ -17,8 +35,9 @@ export function TwitterFollowCard({ addAt, userName, name, itsFollowing }) {
                 </div>
             </header>
             <aside>
-                <button className='tw-followCard-button'>
-                    {text}
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className="tw-followCard-text">{text}</span>
+                    <span className="tw-followCard-unFollow">Dejar de seguir</span>
                 </button>
             </aside>
         </article>
